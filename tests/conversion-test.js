9e98634bc8ecd987dd6fc5f1f412784c493aa7f8
Andras Horvath <horvand@users.noreply.github.com>
2022-09-21 18:56:19 +0200

    JS tests for converting `option` and complex `enum` values

--- tests/conversion-test.js
+++ tests/conversion-test.js
@@ -252,15 +252,24 @@ function test_register_storage() {
 }
 
 function test_enum_with_parameter() {
-  const sch = { "prim": "or",
-          "args":
-            [ { "prim": "or",
-                "args":
-                  [ { "prim": "int", "annots": [ "%decrement" ] },
-                    { "prim": "int", "annots": [ "%increment" ] } ] },
-              { "prim": "unit", "annots": [ "%reset" ] } ] };
-  const data = { "decrement": "1" };
-  const expected = {"prim":"Left","args":[{"prim":"Left","args":[{"int":"1"}]}]};
+  const sch = {
+    prim: "or",
+    args: [
+      {
+        prim: "or",
+        args: [
+          { prim: "int", annots: ["%decrement"] },
+          { prim: "int", annots: ["%increment"] },
+        ],
+      },
+      { prim: "unit", annots: ["%reset"] },
+    ],
+  };
+  const data = { decrement: "1" };
+  const expected = {
+    prim: "Left",
+    args: [{ prim: "Left", args: [{ int: "1" }] }],
+  };
   const encoded = jsonEncode(sch, data);
   print("encoded", JSON.stringify(encoded));
   assert.deepStrictEqual(encoded, expected);
@@ -268,6 +277,173 @@ function test_enum_with_parameter() {
   const back = jsonDecode(sch, encoded);
   print("back", back);
   assert.deepStrictEqual(back, data);
+}
+function test_enum_with_parameter_in_record() {
+  const sch = {
+    prim: "pair",
+    args: [
+      {
+        prim: "or",
+        args: [
+          {
+            prim: "or",
+            args: [
+              { prim: "int", annots: ["%decrement"] },
+              { prim: "int", annots: ["%increment"] },
+            ],
+          },
+          { prim: "unit", annots: ["%reset"] },
+        ],
+        annots: ["%a"],
+      },
+      { prim: "int", annots: ["%i"] },
+    ],
+  };
+  const data = { a: { decrement: "1" }, i: "42" };
+  const expected = {
+    prim: "Pair",
+    args: [
+      { prim: "Left", args: [{ prim: "Left", args: [{ int: "1" }] }] },
+      { int: "42" },
+    ],
+  };
+  const encoded = jsonEncode(sch, data);
+  print("encoded", JSON.stringify(encoded));
+  assert.deepStrictEqual(encoded, expected);
+
+  const back = jsonDecode(sch, encoded);
+  print("back", back);
+  assert.deepStrictEqual(back, data);
+
+  const data2 = { a: { __enum__: "Reset" }, i: "42" };
+  // This below works for encoding, but will be decoded as above
+  // const data2 = { a: {"reset": JsonUnit}, i: "42" };
+  const expected2 = {
+    prim: "Pair",
+    args: [{ prim: "Right", args: [{ prim: "Unit" }] }, { int: "42" }],
+  };
+  const encoded2 = jsonEncode(sch, data2);
+  print("encoded", JSON.stringify(encoded2));
+  assert.deepStrictEqual(encoded2, expected2);
+
+  const back2 = jsonDecode(sch, encoded2);
+  print("back", back2);
+  assert.deepStrictEqual(back2, data2);
+}
+
+function test_adt() {
+  const sch = {
+    prim: "pair",
+    args: [
+      {
+        prim: "or",
+        args: [
+          {
+            prim: "or",
+            args: [
+              {
+                prim: "pair",
+                args: [{ prim: "int" }, { prim: "string" }],
+                annots: ["%decrement"],
+              },
+              {
+                prim: "pair",
+                args: [{ prim: "int" }, { prim: "int" }],
+                annots: ["%increment"],
+              },
+            ],
+          },
+          { prim: "unit", annots: ["%reset"] },
+        ],
+        annots: ["%a"],
+      },
+      { prim: "int", annots: ["%i"] },
+    ],
+  };
+  const data = { a: { decrement: { 0: "1", 1: "foo" } }, i: "42" };
+  const expected = {
+    prim: "Pair",
+    args: [
+      {
+        prim: "Left",
+        args: [
+          {
+            prim: "Left",
+            args: [{ prim: "Pair", args: [{ int: "1" }, { string: "foo" }] }],
+          },
+        ],
+      },
+      { int: "42" },
+    ],
+  };
+  const encoded = jsonEncode(sch, data);
+  print("encoded", JSON.stringify(encoded));
+  assert.deepStrictEqual(encoded, expected);
+
+  const back = jsonDecode(sch, encoded);
+  print("back", back);
+  assert.deepStrictEqual(back, data);
+}
+
+function test_option() {
+  const sch = { prim: "option", args: [{ prim: "int" }], annots: ["%a"] };
+  const data_some = "1";
+  const data_none = null;
+  const expected_some = { prim: "Some", args: [{ int: "1" }] };
+  const expected_none = { prim: "None" };
+
+  const encoded = jsonEncode(sch, data_some);
+  print("encoded", JSON.stringify(encoded));
+  assert.deepStrictEqual(encoded, expected_some);
+
+  const back = jsonDecode(sch, encoded);
+  print("back", back);
+  assert.deepStrictEqual(back, data_some);
+
+  const encoded2 = jsonEncode(sch, data_none);
+  print("encoded", JSON.stringify(encoded2));
+  assert.deepStrictEqual(encoded2, expected_none);
+
+  const back2 = jsonDecode(sch, encoded2);
+  print("back", back2);
+  assert.deepStrictEqual(back2, data_none);
+}
+
+function test_option_in_record() {
+  const sch = {
+    prim: "pair",
+    args: [
+      { prim: "option", args: [{ prim: "int" }], annots: ["%a"] },
+      { prim: "int", annots: ["%b"] },
+    ],
+  };
+  const data_none = { a: null, b: "1" };
+  const data_some = { a: "1", b: "1" };
+
+  const expected_some = {
+    prim: "Pair",
+    args: [{ prim: "Some", args: [{ int: "1" }] }, { int: "1" }],
+  };
+  const expected_none = {
+    prim: "Pair",
+    args: [{ prim: "None" }, { int: "1" }],
+  };
+
+  const encoded = jsonEncode(sch, data_some);
+  print("encoded", JSON.stringify(encoded));
+  assert.deepStrictEqual(encoded, expected_some);
+
+  const back = jsonDecode(sch, encoded);
+  print("back", back);
+  assert.deepStrictEqual(back, data_some);
+
+  const encoded2 = jsonEncode(sch, data_none);
+  print("encoded", JSON.stringify(encoded2));
+  assert.deepStrictEqual(encoded2, expected_none);
+
+  const back2 = jsonDecode(sch, encoded2);
+  print("back", back2);
+  assert.deepStrictEqual(back2, data_none);
 }
 
 function fail() {
@@ -279,4 +455,8 @@ test_unit();
 test_map();
 test_enum();
 test_register_storage();
- test_enum_with_parameter();
+test_enum_with_parameter();
+test_enum_with_parameter_in_record();
+test_adt();
+test_option();
+test_option_in_record();
