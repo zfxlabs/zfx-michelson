8ab94983a7b079376110294c5357be6a4ca566c9
Andras Horvath <horvand@users.noreply.github.com>
2022-09-30 18:31:03 +0200

    Update tests

--- tests/conversion_test.rs
+++ tests/conversion_test.rs
@@ -1,6 +1,7 @@
 /// Execute a NodeJS script and check its exit status
 ///
 /// Fails if `node` is not in the `PATH`
+#[allow(unused)]
 fn js_test_runner(js_file: &str) {
     use std::process::Command;
     // Looks like `cargo test` always executes in the project root directory
@@ -8,7 +9,16 @@ fn js_test_runner(js_file: &str) {
     assert!(st.success());
 }
 
+/// Run `npm test`
+fn npm_test_runner() {
+    use std::process::Command;
+    // Looks like `cargo test` always executes in the project root directory
+    let st = Command::new("npm").arg("test").status().unwrap();
+    assert!(st.success());
+}
+
 #[test]
 fn run_json_conversion_test() {
-    js_test_runner("./tests/conversion-test.js");
+    // js_test_runner("./tests/conversion-test.js");
+    npm_test_runner();
 }
